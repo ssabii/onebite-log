90ee7245c6214690d050bc8197fb4d9c95ab9c13
ssabii <fhy1930@gmail.com>
2026-01-26 21:27:50 +0900

    feat: 회원가입 구현

--- src/pages/sign-up-page.tsx
+++ src/pages/sign-up-page.tsx
@@ -1,3 +1,51 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useSignUp } from "@/hooks/use-sign-up";
+import { useState } from "react";
+import { Link } from "react-router";
+
 export default function SignUpPage() {
-  return <div>Sign Up Page</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { mutate: signUp } = useSignUp();
+
+  const handleSignUpClick = () => {
+    if (email.trim() === "") return;
+    if (password.trim() === "") return;
+
+    signUp({ email, password });
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="text-xl font-bold">회원가입</div>
+      <div className="flex flex-col gap-2">
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="py-6"
+          type="email"
+          placeholder="example@abc.com"
+        />
+        <Input
+          className="py-6"
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button className="w-full" onClick={handleSignUpClick}>
+          회원가입
+        </Button>
+      </div>
+      <div>
+        <Link to="/sign-in" className="text-muted-foreground hover:underline">
+          이미 계정이 있다면? 로그인
+        </Link>
+      </div>
+    </div>
+  );
 }
