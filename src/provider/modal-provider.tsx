65625cb3ee91eba7a1bdf58444a9b2ff83f23669
ssabii <fhy1930@gmail.com>
2026-01-31 15:28:28 +0900

    feat: 포스트 추가 UI 구현

---
+++ src/provider/modal-provider.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+import PostEditorModal from "@/components/modal/post-editor-modal";
+import { createPortal } from "react-dom";
+
+export default function ModalProvider({ children }: { children: ReactNode }) {
+  return (
+    <>
+      {createPortal(
+        <PostEditorModal />,
+        document.getElementById("modal-root")!,
+      )}
+      {children}
+    </>
+  );
+}
