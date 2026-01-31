81890fb45c558c7e317d3a86d932fd8bfa904f38
ssabii <fhy1930@gmail.com>
2026-02-01 07:30:05 +0900

    feat: 이미지 업로드 구현하기 2

--- src/hooks/mutations/post/use-create-post.ts
+++ src/hooks/mutations/post/use-create-post.ts
@@ -1,10 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
-import { createPost } from "@/api/post";
+import { createPostWithImages } from "@/api/post";
 import type { UseMutationCallback } from "@/types";
+import { useMutation } from "@tanstack/react-query";
 
 export function useCreatePost(callbacks?: UseMutationCallback) {
   return useMutation({
-    mutationFn: createPost,
+    mutationFn: createPostWithImages,
     ...callbacks,
   });
 }
