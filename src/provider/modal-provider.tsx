2b53875448c28c8586ed3c667a8d5a3f4df05e1e
ssabii <fhy1930@gmail.com>
2026-02-01 08:10:21 +0900

    feat: 포스트 작성중 이탈 방지 기능 추가

--- src/provider/modal-provider.tsx
+++ src/provider/modal-provider.tsx
@@ -1,12 +1,16 @@
-import type { ReactNode } from "react";
+import AlertModal from "@/components/modal/alert-modal";
 import PostEditorModal from "@/components/modal/post-editor-modal";
+import type { ReactNode } from "react";
 import { createPortal } from "react-dom";
 
 export default function ModalProvider({ children }: { children: ReactNode }) {
   return (
     <>
       {createPortal(
-        <PostEditorModal />,
+        <>
+          <PostEditorModal />
+          <AlertModal />
+        </>,
         document.getElementById("modal-root")!,
       )}
       {children}
