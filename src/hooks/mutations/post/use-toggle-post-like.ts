ff4b00d788fa40e9b8e9fbf358a32ba5ff33f138
ssabii <fhy1930@gmail.com>
2026-02-05 22:28:34 +0900

    feat: 좋아요 결과 반영하기 (낙관적 업데이트)

--- src/hooks/mutations/post/use-toggle-post-like.ts
+++ src/hooks/mutations/post/use-toggle-post-like.ts
@@ -1,10 +1,51 @@
 import { togglePostLike } from "@/api/post";
-import type { UseMutationCallback } from "@/types";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { Post, UseMutationCallback } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useTogglePostLike(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: togglePostLike,
     ...callbacks,
+    onMutate: async ({ postId }) => {
+      callbacks?.onMutate?.();
+
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.post.byId(postId),
+      });
+
+      const prevPost = queryClient.getQueryData<Post>(
+        QUERY_KEYS.post.byId(postId),
+      );
+
+      queryClient.setQueryData<Post>(QUERY_KEYS.post.byId(postId), (post) => {
+        if (!post) throw new Error("포스트가 존재하지 않습니다.");
+        return {
+          ...post,
+          isLiked: !post.isLiked,
+          like_count: post.isLiked ? post.like_count - 1 : post.like_count + 1,
+        };
+      });
+
+      return { prevPost };
+    },
+    onError: (error, _variables, context) => {
+      callbacks?.onError?.(error);
+
+      if (context && context.prevPost) {
+        queryClient.setQueryData(
+          QUERY_KEYS.post.byId(context.prevPost.id),
+          context.prevPost,
+        );
+      }
+    },
+    onSuccess: () => {
+      callbacks?.onSuccess?.();
+    },
+    onSettled: () => {
+      callbacks?.onSettled?.();
+    },
   });
 }
