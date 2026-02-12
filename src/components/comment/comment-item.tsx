e0c29d19bff429264bb98ca494260597361f0943
ssabii <fhy1930@gmail.com>
2026-02-13 02:48:06 +0900

    feat: 댓글 조회 기능 구현하기

--- src/components/comment/comment-item.tsx
+++ src/components/comment/comment-item.tsx
@@ -1,7 +1,11 @@
-import { Link } from "react-router";
 import defaultAvatar from "@/assets/default-avatar.png";
+import { formatTimeAgo } from "@/lib/time";
+import type { Comment } from "@/types";
+import { Link } from "react-router";
 
-export default function CommentItem() {
+export default function CommentItem(props: Comment) {
+  const { content, created_at, author } = props;
+  const { avatar_url, nickname } = author;
   return (
     <div className={"flex flex-col gap-8 border-b pb-5"}>
       <div className="flex items-start gap-4">
@@ -9,18 +13,18 @@ export default function CommentItem() {
           <div className="flex h-full flex-col">
             <img
               className="h-10 w-10 rounded-full object-cover"
-              src={defaultAvatar}
+              src={avatar_url || defaultAvatar}
             />
           </div>
         </Link>
         <div className="flex w-full flex-col gap-2">
-          <div className="font-bold">작성자의 이름</div>
-          <div>댓글 컨텐츠</div>
+          <div className="font-bold">{nickname}</div>
+          <div>{content}</div>
           <div className="text-muted-foreground flex justify-between text-sm">
             <div className="flex items-center gap-2">
               <div className="cursor-pointer hover:underline">댓글</div>
               <div className="bg-border h-[13px] w-[2px]"></div>
-              <div>10분 전</div>
+              <div>{formatTimeAgo(created_at)}</div>
             </div>
             <div className="flex items-center gap-2">
               <div className="cursor-pointer hover:underline">수정</div>
